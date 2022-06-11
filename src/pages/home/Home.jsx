6e57e2e4881eee67eb4d8d4de16765b648911ba8
oneny <gjwodnjs1996@gmail.com>
2022-06-11 11:25:48 +0900

    update mainpage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
+import CollectionTile from '../../components/home/tile/CollectionTile';
+import Masthead from '../../components/home/mastHead/Masthead';
 
 function Home() {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = useCallback(() => {
+    console.log("window.scrollY", window.scrollY);
+    setScrollY(window.scrollY);
+  }, []);
+
+  useEffect(() => { // window.scrollY 감지
+    document.addEventListener("scroll", handleScroll, {
+      passive: true,
+    });
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
   return (
-    <div>메인페이지를 만들자!</div>
+    <>
+      <Masthead scrollY={scrollY} />
+      <CollectionTile scrollY={scrollY} numberOfPage={3} />
+    </>
   )
 }
 
